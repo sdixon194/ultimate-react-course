28f96132a6250d88525e7b9fa4f9e98e3fe6b21f
sdixon194 <steve.dixon@automattic.com>
2024-07-17 09:40:48 -0400

    Projects 8-10

---
+++ 10-react-quiz/react-quiz/src/components/NextButton.js
@@ -0,0 +1,36 @@
+function NextButton({ dispatch, answer, index, numQuestions }) {
+  if (answer === null) return null;
+
+  if (index < numQuestions - 1) {
+    return (
+      <button
+        className="btn btn-ui"
+        onClick={() => dispatch({ type: "nextQuestion" })}
+      >
+        Next
+      </button>
+    );
+  }
+
+  if (index === numQuestions - 1) {
+    return (
+      <button
+        className="btn btn-ui"
+        onClick={() => dispatch({ type: "finish" })}
+      >
+        Finish
+      </button>
+    );
+  }
+
+  return (
+    <button
+      className="btn btn-ui"
+      onClick={() => dispatch({ type: "restart" })}
+    >
+      Restart Quiz
+    </button>
+  );
+}
+
+export default NextButton;
